c1c6df8279eed6c87f758ebbf0a1349d04c783bf
Eoin O'Malley <eomalley@letsgetchecked.com>
2025-07-19 23:52:31 +0100

    [VET-15] Add pages fade transition

--- app/layouts/default.vue
+++ app/layouts/default.vue
@@ -1,5 +1,18 @@
 <template>
 	<main class="n-stack-horizontal">
-		<NuxtPage />
+		<Transition name="fade" mode="out-in" appear>
+			<NuxtPage />
+		</Transition>
 	</main>
 </template>
+
+<style scoped>
+.fade-enter-active,
+.fade-leave-active {
+	transition: opacity 0.4s ease;
+}
+.fade-enter-from,
+.fade-leave-to {
+	opacity: 0;
+}
+</style>
