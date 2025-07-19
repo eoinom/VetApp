8568b35b94df7951e4bc3b22319b47d3366aa725
Eoin O'Malley <eomalley@letsgetchecked.com>
2025-07-20 00:00:11 +0100

    [VET-5 & VET-7] Scaffold index and success pages

---
+++ app/pages/index.vue
@@ -0,0 +1,8 @@
+<script setup lang="ts"></script>
+
+<template>
+	<div>
+		<h1>Sign up page!</h1>
+		<NuxtLink to="/success">Go to Success Page</NuxtLink>
+	</div>
+</template>
---
+++ app/pages/success.vue
@@ -0,0 +1,8 @@
+<script setup lang="ts"></script>
+
+<template>
+	<div>
+		<h1>Success!</h1>
+		<NuxtLink to="/">Back to Sign Up page</NuxtLink>
+	</div>
+</template>
